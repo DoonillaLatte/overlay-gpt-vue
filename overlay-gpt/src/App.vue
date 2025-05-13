81e69546dd53c06b23cf999d434dd78d6d750cf8
JunLennon <jun030516@gmail.com>
2025-05-13 21:16:38 +0900

    edit

--- overlay-gpt/src/App.vue
+++ overlay-gpt/src/App.vue
@@ -11,8 +11,6 @@ export default {
 <template>
   <div class="container">
     <div class="header drag-region">
-      <!-- 여기를 드래그하면 창이 이동합니다 -->
-      <button class="close-btn" @click="closeWindow">X</button>
     </div>
     <div class="main">
       HELLO
