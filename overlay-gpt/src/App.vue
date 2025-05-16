849157d47015141ad8d51361a9bf8a8448a40a73
JunLennon <jun030516@gmail.com>
2025-05-16 11:41:00 +0900

    update

--- overlay-gpt/src/App.vue
+++ overlay-gpt/src/App.vue
@@ -27,10 +27,22 @@ export default {
       HELLO
     </div>
     <div class="prompt-region">
-      <div></div>
-      <input type="text" class="prompt">
-      </input>
-      <div></div>
+      <div class="prompt-container">
+        <div class="upload-button-wrapper">
+          <button type="button" class="upload-button">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M12 5V19M5 12H19" stroke="white" stroke-width="2" stroke-linecap="round"/>
+            </svg>
+          </button>
+          <div class="tooltip">사진 및 파일 추가</div>
+        </div>
+        <input type="text" class="prompt" placeholder="메시지 입력...">
+        <button type="submit" class="submit-button">
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12 5L12 19M12 5L5 12M12 5L19 12" stroke="#303030" stroke-width="5" stroke-linecap="round"/>
+          </svg>
+        </button>
+      </div>
     </div>
   </div>
 </template>
